ca60d792d8a7f187365c7e44107ad12f09d14578
Devilcoder2 <ramandeepsingh40270@gmail.com>
2023-04-23 13:23:49 +0530

    gallery css changed to moduled css

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import "./Gallery.css";
+import classes from "./Gallery.module.css";
+
 import image1 from "../assets/Images/1.jpg";
 import image2 from "../assets/Images/2 (2).jpg";
 import image3 from "../assets/Images/3.jpg";
@@ -22,81 +23,81 @@ import image19 from "../assets/Images/19.jpg";
 
 const Gallery = () => {
   return (
-    <div className="grid-wrapper">
+    <div className={classes.grid - wrapper}>
       <div>
-        <img className="gallery_img" src={image1} alt="Photo1" />
+        <img className={classes.gallery_img} src={image1} alt="Photo1" />
       </div>
 
-      <div className="wide">
-        <img className="gallery_img" src={image2} alt="Photo2" />
-      </div>
-
-      <div>
-        <img className="gallery_img" src={image3} alt="Photo3" />
-      </div>
-
-      <div className="big">
-        <img className="gallery_img" src={image4} alt="Photo4" />
+      <div className={classes.wide}>
+        <img className={classes.gallery_img} src={image2} alt="Photo2" />
       </div>
 
       <div>
-        <img className="gallery_img" src={image5} alt="Photo5" />
+        <img className={classes.gallery_img} src={image3} alt="Photo3" />
       </div>
 
-      <div className="wide">
-        <img className="gallery_img" src={image6} alt="Photo6" />
-      </div>
-
-      <div className="tall">
-        <img className="gallery_img" src={image7} alt="Photo7" />
-      </div>
-
-      <div className="tall">
-        <img className="gallery_img" src={image8} alt="Photo8" />
+      <div className={classes.big}>
+        <img className={classes.gallery_img} src={image4} alt="Photo4" />
       </div>
 
       <div>
-        <img className="gallery_img" src={image9} alt="Photo9" />
+        <img className={classes.gallery_img} src={image5} alt="Photo5" />
       </div>
 
-      <div className="big">
-        <img className="gallery_img" src={image10} alt="Photo10" />
+      <div className={classes.wide}>
+        <img className={classes.gallery_img} src={image6} alt="Photo6" />
       </div>
 
-      <div>
-        <img className="gallery_img" src={image11} alt="Photo11" />
+      <div className={classes.tall}>
+        <img className={classes.gallery_img} src={image7} alt="Photo7" />
       </div>
 
-      <div className="wide">
-        <img className="gallery_img" src={image12} alt="Photo12" />
-      </div>
-
-      <div className="wide">
-        <img className="gallery_img" src={image13} alt="Photo13" />
-      </div>
-
-      <div className="tall">
-        <img className="gallery_img" src={image14} alt="Photo14" />
-      </div>
-
-      <div className="wide">
-        <img className="gallery_img" src={image15} alt="Photo15" />
-      </div>
-
-      <div className="tall">
-        <img className="gallery_img" src={image16} alt="Photo16" />
+      <div className={classes.tall}>
+        <img className={classes.gallery_img} src={image8} alt="Photo8" />
       </div>
 
       <div>
-        <img className="gallery_img" src={image17} alt="Photo17" />
+        <img className={classes.gallery_img} src={image9} alt="Photo9" />
+      </div>
+
+      <div className={classes.big}>
+        <img className={classes.gallery_img} src={image10} alt="Photo10" />
       </div>
 
       <div>
-        <img className="gallery_img" src={image18} alt="Photo18" />
+        <img className={classes.gallery_img} src={image11} alt="Photo11" />
       </div>
 
-      <div className="tall">
-        <img className="gallery_img" src={image19} alt="Photo19" />
+      <div className={classes.wide}>
+        <img className={classes.gallery_img} src={image12} alt="Photo12" />
+      </div>
+
+      <div className={classes.wide}>
+        <img className={classes.gallery_img} src={image13} alt="Photo13" />
+      </div>
+
+      <div className={classes.tall}>
+        <img className={classes.gallery_img} src={image14} alt="Photo14" />
+      </div>
+
+      <div className={classes.wide}>
+        <img className={classes.gallery_img} src={image15} alt="Photo15" />
+      </div>
+
+      <div className={classes.tall}>
+        <img className={classes.gallery_img} src={image16} alt="Photo16" />
+      </div>
+
+      <div>
+        <img className={classes.gallery_img} src={image17} alt="Photo17" />
+      </div>
+
+      <div>
+        <img className={classes.gallery_img} src={image18} alt="Photo18" />
+      </div>
+
+      <div className={classes.tall}>
+        <img className={classes.gallery_img} src={image19} alt="Photo19" />
       </div>
     </div>
   );
